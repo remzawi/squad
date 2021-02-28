fdecd9febcb1ca3e57993e59d49b485d9d90b195
remzawi <remzawi@gmail.com>
2021-02-27 20:26:46 -0800

    try optim

--- train.py
+++ train.py
@@ -86,20 +86,20 @@ def main(args):
                                  log=log)
 
     # Get optimizer and scheduler
-    #if args.name == 'qanet' :
+    if args.name == 'qanet' :
         #optimizer = optim.Adam(model.parameters(), args.lr,
         #                       betas=(0.8, 0.999),
         #                       weight_decay=3*1e-7,
         #                       eps=1e-7)
-    #    optimizer = AdamW(model.parameters(), args.lr,
-    #                           betas=(0.8, 0.999),
-    #                           weight_decay=3*1e-7,
-    #                           eps=1e-7)
-    #    scheduler = warmup(optimizer, 1, 1000)
-    #else:
-    optimizer = optim.Adadelta(model.parameters(), args.lr,
+        optimizer = AdamW(model.parameters(), args.lr,
+                               betas=(0.8, 0.999),
+                               weight_decay=3*1e-7,
+                               eps=1e-7)
+        scheduler = warmup(optimizer, 1, 1000)
+    else:
+        optimizer = optim.Adadelta(model.parameters(), args.lr,
                                    weight_decay=3*1e-7)
-    scheduler = sched.LambdaLR(optimizer, lambda s: 1.)  # Constant LR
+        scheduler = sched.LambdaLR(optimizer, lambda s: 1.)  # Constant LR
 
     # Get data loader
     log.info('Building dataset...')
